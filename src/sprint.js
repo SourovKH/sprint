519b779ee3b3d5a77215494e8196b6574f8067b5
Sourov <SourovKH@users.noreply.github.com>
2023-04-28 10:35:29 +0530

    added a function to load memory and added test

--- src/sprint.js
+++ src/sprint.js
@@ -1,30 +1,51 @@
-const assign = function(memory, programCounter) {
+const program = [
+  0, 1, 7, 
+  0, 2, 8,
+  9
+];
+
+const assign = function(state) {
+  const {memory, programCounter} = state;
   const value = memory[programCounter + 1];
   const cellToStore = memory[programCounter + 2];
   memory[cellToStore] = value;
+  const newState = {memory: memory, programCounter: programCounter + 3};
 
-  return [memory, programCounter + 3];
+  return newState;
 }
 
-const instructions = {
-  0: assign,
-  9: "halt"
+const loadMemory = function(program, size) {
+  let memory = [...program];
+  const remainingSize = size - memory.length;
+  const emptyMemory = [];
+  emptyMemory.length = remainingSize;
+  emptyMemory.fill(null);
+  memory = memory.concat(emptyMemory);
+
+  return memory;
 }
 
 const sprint = function(program) {
-  let memory = [...program];
+  const instructions = {
+    0: assign,
+    9: "halt"
+  };
+
+  const memorySize = 10;
+  let memory = loadMemory(program, memorySize);
   let programCounter = 0;
   let instruction = instructions[memory[programCounter]];
 
   while(instruction !== "halt") {
-    [memory, programCounter] = [...assign(memory, programCounter)];
+    let state = {memory: memory, programCounter: programCounter};
+    state = instruction(state);
+    memory = state.memory;
+    programCounter = state.programCounter;
     instruction = instructions[memory[programCounter]];
   }
 
   return memory;
 }
-
-const program = [0, 1, 5, 9];
 
 console.log(sprint(program));
 
--- test/sprint-test.js
+++ test/sprint-test.js
@@ -1,11 +1,13 @@
 const deepStrictEqual = require('assert');
 const {describe, it} = require('node:test');
-const object = require("../src/sprint.js")
-
-const sprint = object.sprint;
+const {sprint} = require("../src/sprint.js")
 
 describe("sprint", function() {
-  it("should be true", function() {
+  it("should halt at the start", function() {
     deepStrictEqual([9], sprint([9]));
+  });
+
+  it("should assign a value to a memory location", function() {
+    deepStrictEqual(sprint([0, 1, 5, 9]), [0, 1, 5, 9, null, 1, null, null, null, null]);
   });
 });
